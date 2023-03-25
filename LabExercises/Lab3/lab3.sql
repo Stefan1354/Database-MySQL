be725e7ba71838f300a9a8f10252fb00e3dc2a46
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-03-25 17:14:43 +0200

    Add files via upload

---
+++ LabExercises/Lab3/lab3.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS school_sport_clubs;
+CREATE DATABASE school_sport_clubs;
+USE school_sport_clubs;
+
+CREATE TABLE school_sport_clubs.sports(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE school_sport_clubs.coaches(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	name VARCHAR(255) NOT NULL ,
+	egn VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE school_sport_clubs.students(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	name VARCHAR(255) NOT NULL ,
+	egn VARCHAR(10) NOT NULL UNIQUE ,
+	address VARCHAR(255) NOT NULL ,
+	phone VARCHAR(20) NULL DEFAULT NULL ,
+	class VARCHAR(10) NULL DEFAULT NULL   
+);
+
+CREATE TABLE school_sport_clubs.sportGroups(
+	id INT AUTO_INCREMENT PRIMARY KEY ,
+	location VARCHAR(255) NOT NULL ,
+	dayOfWeek ENUM('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday') ,
+	hourOfTraining TIME NOT NULL ,
+	sport_id INT NULL ,
+	coach_id INT NULL ,
+	UNIQUE KEY(location,dayOfWeek,hourOfTraining)  ,
+	CONSTRAINT FOREIGN KEY(sport_id) 
+		REFERENCES sports(id) ,
+	CONSTRAINT FOREIGN KEY (coach_id) 
+		REFERENCES coaches(id)
+);
+
+CREATE TABLE school_sport_clubs.student_sport(
+	student_id INT NOT NULL , 
+	sportGroup_id INT NOT NULL ,  
+	CONSTRAINT FOREIGN KEY (student_id) 
+		REFERENCES students(id) ,	
+	CONSTRAINT FOREIGN KEY (sportGroup_id) 
+		REFERENCES sportGroups(id) ,
+	PRIMARY KEY(student_id,sportGroup_id)
+);
+
+SELECT students.name, sports.name
+FROM students
+INNER JOIN student_sport ON student_sport.student_id = students.id
+INNER JOIN sportgroups ON sportgroups.id = sportGroup_id
+INNER JOIN sports ON sports.id = sportgroups.sport_id
+
+
+
+
+
+
+
+
+
