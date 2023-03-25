4429b4e9f1ac0e21102ba266b0aab1b827d3b79c
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-03-25 17:26:45 +0200

    Update lab3.sql

--- LabExercises/Lab3/lab3.sql
+++ LabExercises/Lab3/lab3.sql
@@ -46,17 +46,57 @@ CREATE TABLE school_sport_clubs.student_sport(
 	PRIMARY KEY(student_id,sportGroup_id)
 );
 
+#1
+INSERT INTO students (name, egn, address, class, phone)
+VALUES ('Ivan Ivanov Ivanov', '9207186371', 'Sofia-Serdika', '10', '0888892950');
+
+#2
+SELECT * FROM students ORDER BY name;
+
+#3
+DELETE FROM students
+WHERE name = 'Ivan Ivanov Ivanov' AND egn = '9207186371' AND address = 'Sofia-Serdika' AND class = '10' AND phone = '0888892950';
+
+#4
 SELECT students.name, sports.name
 FROM students
-INNER JOIN student_sport ON student_sport.student_id = students.id
-INNER JOIN sportgroups ON sportgroups.id = sportGroup_id
-INNER JOIN sports ON sports.id = sportgroups.sport_id
+JOIN student_sport ON student_sport.student_id = students.id
+JOIN sportGroups ON student_sport.sportGroup_id = sportGroups.id
+JOIN sports ON sportGroups.sport_id = sports.id;
 
+#5
+SELECT students.name, students.class, student_sport.sportGroup_id
+FROM students 
+JOIN student_sport ON students.id = student_sport.student_id
+JOIN sportGroups ON student_sport.sportGroup_id = sportGroups.id
+WHERE sportGroups.dayOfWeek = 'Monday';
 
+#6
+SELECT DISTINCT coaches.name
+FROM coaches
+JOIN sportGroups ON sportGroups.coach_id = coaches.id
+JOIN sports ON sportGroups.sport_id = sports.id
+WHERE sports.name = 'Football';
 
+#7
+SELECT location, dayOfWeek, hourOfTraining
+FROM sportGroups
+JOIN sports ON sports.id = sportGroups.sport_id
+WHERE sports.name = 'Volleyball';
 
+#8
+SELECT sports.name
+FROM sports
+JOIN sportGroups ON sports.id = sportGroups.sport_id
+JOIN student_sport ON sportGroups.id = student_sport.sportGroup_id
+JOIN students ON student_sport.student_id = students.id
+WHERE students.name = 'Iliyan Ivanov';
 
-
-
-
-
+#9
+SELECT students.name
+FROM students
+JOIN student_sport ON students.id = student_sport.student_id
+JOIN sportGroups ON student_sport.sportGroup_id = sportGroups.id
+JOIN coaches ON sportGroups.coach_id = coaches.id
+JOIN sports ON sportGroups.sport_id = sports.id 
+WHERE sports.name = 'Football' AND coaches.name = 'Ivan Todorov Petkov';
