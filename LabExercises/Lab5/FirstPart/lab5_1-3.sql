947be490e3ccb8ea1c67aa9d5357899b9237186a
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-03-31 22:03:45 +0300

    Add files via upload

---
+++ LabExercises/Lab5/FirstPart/lab5_1-3.sql
@@ -0,0 +1,174 @@
+DROP DATABASE IF EXISTS test_cinemas;
+CREATE DATABASE test_cinemas;
+USE test_cinemas;
+
+
+CREATE TABLE projection (
+id INT AUTO_INCREMENT PRIMARY KEY,
+audience INT NOT NULL,
+duration INT NOT NULL
+);
+
+CREATE TABLE films (
+id INT AUTO_INCREMENT PRIMARY KEY,
+year DATE NOT NULL,
+country VARCHAR(255) NOT NULL,
+length INT NOT NULL,
+name VARCHAR(255) NOT NULL,
+producer VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE cinema (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+place VARCHAR(255) NOT NULL,
+no_halls INT NOT NULL
+);
+
+CREATE TABLE halls (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(255) NOT NULL,
+capacity INT NOT NULL,
+status ENUM ('VIP', 'DELUXE', '4D'),
+projection_id INT NOT NULL,
+films_id INT NOT NULL,
+cinema_id INT NOT NULL,
+CONSTRAINT FOREIGN KEY (projection_id) REFERENCES projection(id),
+CONSTRAINT FOREIGN KEY (films_id) REFERENCES films(id),
+CONSTRAINT FOREIGN KEY (cinema_id) REFERENCES cinema(id)
+);
+
+CREATE TABLE films_cinema (
+films_id INT NOT NULL,
+cinema_id INT NOT NULL,
+CONSTRAINT FOREIGN KEY (films_id) REFERENCES films(id),
+CONSTRAINT FOREIGN KEY (cinema_id) REFERENCES cinema(id),
+PRIMARY KEY (films_id, cinema_id)
+);
+
+CREATE TABLE projection_cinema (
+projection_id INT NOT NULL,
+cinema_id INT NOT NULL,
+CONSTRAINT FOREIGN KEY (projection_id) REFERENCES projection(id),
+CONSTRAINT FOREIGN KEY (cinema_id) REFERENCES cinema(id),
+PRIMARY KEY(projection_id, cinema_id)
+);
+
+/*INSERT INTO projection(id, audience, duration)
+VALUES (NULL, 200, 120),
+	   (NULL, 300, 150),
+       (NULL, 400, 90),
+       (NULL, 100, 45),
+       (NULL, 600, 135);
+       
+INSERT INTO films (id, year, country, length, name, producer)
+VALUES (NULL, '2023-03-20', 'Bulgaria', 120, 'Final Destination 7', 'Erik Jons'),
+	   (NULL, '2023-03-21', 'New Zealand', 145, 'The lord of the rings', 'Peter Jackson'),
+       (NULL, '2023-03-22', 'Poland', 120, 'Final Destination 7', 'Erik Jons'),
+       (NULL, '2023-03-23', 'Slovenia', 150, 'Fast and Furiors', 'Elizabet Jones'),
+       (NULL, '2023-03-24', 'Sweden', 120, 'Final Destination 7', 'Erik Jons');
+       
+INSERT INTO cinema (id, name, place, no_halls)
+VALUES (NULL, 'Ivan Vazov', 'Sofia', 1),
+	   (NULL, 'IMAX', 'Berlin', 1),
+       (NULL, 'Arena Mladost', 'Sofia', 3),
+       (NULL, 'Cinemark', 'London', 2),
+       (NULL, 'Regal Cinemas', 'Kaunas', 4);
+       
+INSERT INTO halls (id, name, status, capacity, projection_id, films_id, cinema_id)
+VALUES (NULL, 'Ivan Beikiarov', 'VIP', 300, 2, 1, 4),
+	   (NULL, 'George Orwel', '4D', 800, 1, 3, 5),
+       (NULL, 'Patrik Beverly', 'VIP', 500, 4, 5, 2),
+       (NULL, 'John Smith', 'VIP', 600, 5, 2, 4),
+       (NULL, 'Paul Patrick', '4D', 1300, 3, 4, 4);
+	  
+INSERT INTO films_cinema
+VALUES (2, 5),
+       (3, 4),
+       (1, 2),
+       (4, 3),
+       (2, 1);
+
+INSERT INTO projection_cinema
+VALUES (5, 2),
+       (1, 1),
+       (4, 2),
+       (3, 1),
+       (5, 4);
+       
+*/
+
+
+
+INSERT INTO projection (audience, duration) VALUES
+(50, 120),
+(100, 110),
+(70, 100),
+(60, 95);
+
+-- Запълване на таблица films
+INSERT INTO films (year, country, length, name, producer) VALUES
+('2023-01-01', 'USA', 110, 'Final Destination 7', 'James Wong'),
+('2022-02-14', 'USA', 120, 'A Quiet Place Part II', 'John Krasinski'),
+('2021-05-20', 'USA', 140, 'Godzilla vs. Kong', 'Adam Wingard'),
+('2021-11-24', 'USA', 130, 'Encanto', 'Byron Howard');
+
+-- Запълване на таблица cinema
+INSERT INTO cinema (name, place, no_halls) 
+VALUES ('Arena Mladost', 'Sofia, Bulgaria', 5),
+	   ('Cine Grand', 'Varna, Bulgaria', 4),
+       ('Cineplex', 'Plovdiv, Bulgaria', 6),
+       ('Cinema City', 'Burgas, Bulgaria', 3);
+
+-- Запълване на таблица halls
+INSERT INTO halls (name, capacity, status, projection_id, films_id, cinema_id) VALUES
+('Hall 1', 100, 'VIP', 1, 1, 1),
+('Hall 2', 80, 'DELUXE', 2, 2, 2),
+('Hall 3', 60, '4D', 3, 3, 3),
+('Hall 4', 120, 'VIP', 4, 4, 4),
+('Hall 5', 90, 'DELUXE', 1, 1, 2),
+('Hall 6', 70, '4D', 2, 2, 3),
+('Hall 7', 100, 'VIP', 3, 3, 4),
+('Hall 8', 80, 'DELUXE', 4, 4, 1),
+('Hall 9', 60, '4D', 1, 1, 3),
+('Hall 10', 120, 'VIP', 2, 2, 4),
+('Hall 11', 90, 'DELUXE', 3, 3, 1),
+('Hall 12', 70, '4D', 4, 4, 2);
+
+-- Запълване на таблица films_cinema
+INSERT INTO films_cinema (films_id, cinema_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 2),
+(2, 3),
+(3, 3),
+(3, 4),
+(4, 1),
+(4, 4);
+
+-- Запълване на таблица projection_cinema
+INSERT INTO projection_cinema (projection_id, cinema_id) VALUES
+(1, 1),
+(1, 2),
+(2, 2),
+(2, 3),
+(3, 3),
+(3, 4),
+(4, 1),
+(4, 4);
+
+/*SELECT SUM(projection.audience) AS peopleNumber FROM projection
+JOIN halls ON projection.id = halls.projection_id
+JOIN films ON halls.films_id = films.id
+JOIN films_cinema ON films.id = films_cinema.films_id
+JOIN cinema ON films_cinema.cinema_id = cinema.id
+WHERE films.name = 'Final Destination 7' AND
+halls.status = 'VIP'  AND cinema.name = 'Arena Mladost';*/
+
+/*SELECT cinema.name, halls.id, projection.duration
+FROM cinema JOIN halls ON cinema.id = halls.cinema_id
+JOIN projection ON halls.projection_id = projection.id
+JOIN films ON halls.films_id = films.id
+WHERE films.name = 'Final Destination 7' AND (halls.status = 'VIP' OR halls.status = 'Deluxe')
+ORDER BY cinema.name, halls.id;*/
