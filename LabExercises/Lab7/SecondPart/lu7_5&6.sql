b7dc47c8d57cb7c80c4306ac13b7fe9510ee76ea
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-04-16 15:06:59 +0300

    Update lu7_5&6.sql

--- LabExercises/Lab7/SecondPart/lu7_5&6.sql
+++ LabExercises/Lab7/SecondPart/lu7_5&6.sql
@@ -1,48 +1,46 @@
 CREATE DATABASE IF NOT EXISTS transaction_test;  
-	USE transaction_test;  
+USE transaction_test;  
+DROP TABLE IF EXISTS customer_accounts;  	  
+DROP TABLE IF EXISTS customers;  
 	  
-	DROP TABLE IF EXISTS customer_accounts;  
+CREATE TABLE customers(  
+id int AUTO_INCREMENT PRIMARY KEY ,  
+name VARCHAR(255) NOT NULL ,  
+address VARCHAR(255)  
+)ENGINE=InnoDB;  
 	  
-	DROP TABLE IF EXISTS customers;  
+CREATE TABLE IF NOT EXISTS customer_accounts(  
+id INT AUTO_INCREMENT PRIMARY KEY ,  
+amount DOUBLE NOT NULL ,  
+currency VARCHAR(10),  
+customer_id INT NOT NULL ,  
+CONSTRAINT FOREIGN KEY (customer_id)   
+REFERENCES customers(id)   
+ON DELETE RESTRICT ON UPDATE CASCADE  
+)ENGINE=InnoDB;  
 	  
-	CREATE TABLE customers(  
-	id int AUTO_INCREMENT PRIMARY KEY ,  
-	name VARCHAR(255) NOT NULL ,  
-	address VARCHAR(255)  
-	)ENGINE=InnoDB;  
+INSERT INTO `transaction_test`.`customers` (`name`, `address`)   
+VALUES ('Ivan Petrov Iordanov', 'Sofia, Krasno selo 1000');  
+INSERT INTO `transaction_test`.`customers` (`name`, `address`)   
+VALUES ('Stoyan Pavlov Pavlov', 'Sofia, Liuylin 7, bl. 34');  
+INSERT INTO `transaction_test`.`customers` (`name`, `address`)   
+VALUES ('Iliya Mladenov Mladenov', 'Sofia, Nadezhda 2, bl 33');  
 	  
-	CREATE TABLE IF NOT EXISTS customer_accounts(  
-	id INT AUTO_INCREMENT PRIMARY KEY ,  
-	amount DOUBLE NOT NULL ,  
-	currency VARCHAR(10),  
-	customer_id INT NOT NULL ,  
-	CONSTRAINT FOREIGN KEY (customer_id)   
-	    REFERENCES customers(id)   
-	    ON DELETE RESTRICT ON UPDATE CASCADE  
-	)ENGINE=InnoDB;  
-	  
-	INSERT INTO `transaction_test`.`customers` (`name`, `address`)   
-	VALUES ('Ivan Petrov Iordanov', 'Sofia, Krasno selo 1000');  
-	INSERT INTO `transaction_test`.`customers` (`name`, `address`)   
-	VALUES ('Stoyan Pavlov Pavlov', 'Sofia, Liuylin 7, bl. 34');  
-	INSERT INTO `transaction_test`.`customers` (`name`, `address`)   
-	VALUES ('Iliya Mladenov Mladenov', 'Sofia, Nadezhda 2, bl 33');  
-	  
-	INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
-	VALUES ('5000', 'BGN', '1');  
-	INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
-	VALUES ('10850', 'EUR', '1');  
-	INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
-	VALUES ('1450000', 'BGN', '2');  
-	INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
-	VALUES ('17850', 'EUR', '2');  
+INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
+VALUES ('5000', 'BGN', '1');  
+INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
+VALUES ('10850', 'EUR', '1');  
+INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
+VALUES ('1450000', 'BGN', '2');  
+INSERT INTO `transaction_test`.`customer_accounts` (`amount`, `currency`, `customer_id`)   
+VALUES ('17850', 'EUR', '2');  
     
 
 #1
 DELIMITER //
 DROP PROCEDURE IF EXISTS transfer_money;
 CREATE PROCEDURE transfer_money(IN from_acc_id INT, IN to_acc_id INT, IN transferAmount DECIMAL(10,2))
-/*BEGIN
+BEGIN
     DECLARE from_acc_balance DECIMAL(10, 2);
     DECLARE to_acc_balance DECIMAL(10, 2);
 
@@ -75,7 +73,8 @@ END //
 
 DELIMITER ;
 
-CALL transfer_money(1, 2, 5000);*/
+CALL transfer_money(1, 2, 5000);
+
 
 #2
 DELIMITER //
