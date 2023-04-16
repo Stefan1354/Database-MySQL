dfc8c41d076cc771b74729df12a10491f69afc3c
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-04-16 15:10:20 +0300

    Update lu7_5&6.sql

--- LabExercises/Lab7/SecondPart/lu7_5&6.sql
+++ LabExercises/Lab7/SecondPart/lu7_5&6.sql
@@ -46,24 +46,26 @@ BEGIN
 
     START TRANSACTION;
 
-    SELECT amount INTO from_acc_balance FROM customer_accounts WHERE id = from_acc_id FOR UPDATE;
-    SELECT amount INTO to_acc_balance FROM customer_accounts WHERE id = to_acc_id FOR UPDATE;
+SELECT amount INTO from_acc_balance FROM customer_accounts 
+WHERE id = from_acc_id FOR UPDATE;
+SELECT amount INTO to_acc_balance FROM customer_accounts 
+WHERE id = to_acc_id FOR UPDATE;
 
-    IF from_acc_balance < transferAmount THEN
-        SET @error_message = 'Недостатъчно пари на сметката за трансфер.';
-    ELSE
+IF from_acc_balance < transferAmount THEN
+   SET @error_message = 'Недостатъчно пари на сметката за трансфер.';
+     ELSE
         UPDATE customer_accounts SET amount = amount - transferAmount WHERE id = from_acc_id;
-        IF ROW_COUNT() = 0 THEN
-            SET @error_message = 'Неуспешна транзакция.';
-            ROLLBACK;
-            SELECT @error_message;
-        ELSE
-            UPDATE customer_accounts SET amount = amount + transferAmount WHERE id = to_acc_id;
-            IF ROW_COUNT() = 0 THEN
+           IF ROW_COUNT() = 0 THEN
+              SET @error_message = 'Неуспешна транзакция.';
+              ROLLBACK;
+                SELECT @error_message;
+           ELSE
+                UPDATE customer_accounts SET amount = amount + transferAmount WHERE id = to_acc_id;
+           IF ROW_COUNT() = 0 THEN
                 SET @error_message = 'Неуспешна транзакция.';
                 ROLLBACK;
                 SELECT @error_message;
-            ELSE
+           ELSE
                 COMMIT;
                 SELECT 'Транзакцията беше успешна.';
             END IF;
@@ -80,29 +82,33 @@ CALL transfer_money(1, 2, 5000);
 DELIMITER //
 DROP PROCEDURE IF EXISTS transfer_money;
 CREATE PROCEDURE transfer_money(
-    IN sender_name VARCHAR(255),
-    IN recipient_name VARCHAR(255),
-    IN transferAmount DOUBLE,
-    IN currency VARCHAR(10)
+IN sender_name VARCHAR(255),
+IN recipient_name VARCHAR(255),
+IN transferAmount DOUBLE,
+IN currency VARCHAR(10)
 )
 BEGIN
     DECLARE sender_id, recipient_id, affected_rows INT;
     DECLARE sender_balance, recipient_balance DOUBLE;
     
-    SELECT id INTO sender_id FROM customers WHERE name = sender_name;
-    SELECT id INTO recipient_id FROM customers WHERE name = recipient_name;
+SELECT id INTO sender_id FROM customers 
+WHERE name = sender_name;
+SELECT id INTO recipient_id FROM customers 
+WHERE name = recipient_name;
     
-    SELECT transferAmount INTO sender_balance FROM customer_accounts WHERE customer_id = sender_id AND currency = currency;
-    SELECT transferAmount INTO recipient_balance FROM customer_accounts WHERE customer_id = recipient_id AND currency = currency;
+SELECT transferAmount INTO sender_balance FROM customer_accounts 
+WHERE customer_id = sender_id AND currency = currency;
+SELECT transferAmount INTO recipient_balance FROM customer_accounts 
+WHERE customer_id = recipient_id AND currency = currency;
     
-    IF sender_balance < amount THEN
-        SELECT 'Not enough funds' AS error_message;
-    ELSE
-        UPDATE customer_accounts SET transferAmount = sender_balance - transferAmount WHERE customer_id = sender_id AND currency = currency;
-        SET affected_rows = ROW_COUNT();
-        IF affected_rows = 0 THEN
+IF sender_balance < amount THEN
+      SELECT 'Not enough funds' AS error_message;
+ELSE
+      UPDATE customer_accounts SET transferAmount = sender_balance - transferAmount WHERE customer_id = sender_id AND currency = currency;
+      SET affected_rows = ROW_COUNT();
+      IF affected_rows = 0 THEN
             SELECT 'Transaction failed' AS error_message;
-        ELSE
+      ELSE
             UPDATE customer_accounts SET transferAmount = recipient_balance + transferAmount WHERE customer_id = recipient_id AND currency = currency;
             SET affected_rows = ROW_COUNT();
             IF affected_rows = 0 THEN
