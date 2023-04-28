ddb5d142455523af745674cd06a432fd9bab4a06
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-04-28 23:20:24 +0300

    Update lu8_4-5.sql

--- LabExercises/Lab8/SecondPart/lu8_4-5.sql
+++ LabExercises/Lab8/SecondPart/lu8_4-5.sql
@@ -37,7 +37,7 @@ DROP TABLE IF EXISTS customers;
 
 
 #4
-/*DELIMITER |
+DELIMITER |
 DROP PROCEDURE IF EXISTS converter;
 CREATE PROCEDURE converter (IN amount DOUBLE, IN currency VARCHAR(5), OUT returnAmount DOUBLE)
 BEGIN
@@ -53,7 +53,7 @@ END;
 |
 DELIMITER ;
 
-CALL converter('1000.00', 'BGN', @returnAmount)*/
+CALL converter('1000.00', 'BGN', @returnAmount)
 
 
 #5
@@ -61,46 +61,42 @@ DROP PROCEDURE IF EXISTS transactionIds;
 DELIMITER |
 CREATE PROCEDURE transactionIds(IN firstId INT, IN secondId INT, IN transferAmount DOUBLE)
 BEGIN
-	DECLARE firstCurrency VARCHAR(5);
+    DECLARE firstCurrency VARCHAR(5);
     DECLARE secondCurrency VARCHAR(5);
     
-    SELECT currency
-    INTO firstCurrency
-    FROM customer_accounts
+    SELECT currency INTO firstCurrency
+    FROM customer_accounts 
     WHERE id = firstId;
     
-    SELECT currency
-    INTO secondCurrency
+    SELECT currency INTO secondCurrency
     FROM customer_accounts
     WHERE id = secondId;
     
     IF ((firstCurrency != 'BGN' AND firstCurrency != 'EUR') AND (secondCurrency != 'BGN' AND secondCurrency != 'EUR'))
     THEN
-		SELECT "Currencies must be either 'BGN' or 'EUR'!";
-	ELSE
-		IF ((SELECT amount
-			FROM customer_accounts
-			WHERE id = firstId) - transferAmount < 0)
-		THEN
-			SELECT "Not enough money to withdraw!";
-		ELSE
-			START TRANSACTION;
-				UPDATE customer_accounts
+        SELECT "Currencies must be either 'BGN' or 'EUR'!";
+    ELSE
+        IF ((SELECT amount FROM customer_accounts WHERE id = firstId) - transferAmount < 0)
+        THEN
+            SELECT "Not enough money to withdraw!";
+        ELSE
+            START TRANSACTION;
+                UPDATE customer_accounts
                 SET amount = amount - transferAmount
                 WHERE id = firstId;
                 
                 IF (ROW_COUNT() = 0)
                 THEN
-					SELECT "Transaction couldn't execute!";
+                    SELECT "Transaction couldn't execute!";
                     ROLLBACK;
-				ELSE
-					IF (firstCurrency != secondCurrency)
+                ELSE
+                    IF (firstCurrency != secondCurrency)
                     THEN
-						SET @returnAmount = 0;
+                        SET @returnAmount = 0;
                         CALL converter(transferAmount, firstCurrency, @returnAmount);
-					ELSE
-						SET @returnAmount = transferAmount;
-					END IF;
+                    ELSE
+                        SET @returnAmount = transferAmount;
+                    END IF;
                     
                     UPDATE customer_accounts
                     SET amount = amount + @returnAmount
@@ -108,14 +104,14 @@ BEGIN
                     
                     IF (ROW_COUNT() = 0)
                     THEN
-						SELECT "Transaction couldn't execute!";
+                        SELECT "Transaction couldn't execute!";
                         ROLLBACK;
-					ELSE
-						COMMIT;
-					END IF;
-				END IF;
-			END IF;
-		END IF;
+                    ELSE
+                        COMMIT;
+                    END IF;
+                END IF;
+            END IF;
+        END IF;
 END;
 |
 DELIMITER ;
