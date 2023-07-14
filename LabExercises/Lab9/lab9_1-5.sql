c3a907e6c164c0a848b8b883a33494b621c6c83a
Stefan Stoilkov <stefanstoilkov02@gmail.com>
2023-07-14 22:15:28 +0300

    Update lab9_1-5.sql

--- LabExercises/Lab9/lab9_1-5.sql
+++ LabExercises/Lab9/lab9_1-5.sql
@@ -3,7 +3,7 @@ CREATE DATABASE school_sport_clubs;
 USE school_sport_clubs;
 
 CREATE TABLE school_sport_clubs.sports(
-	id INT AUTO_INCREMENT PRIMARY KEY ,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(255) NOT NULL
 ) Engine = Innodb;
 
